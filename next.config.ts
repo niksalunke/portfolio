7c427dc4de53540f9e63ee41a157a05257531b2c
Nikhil Salunke <ntsalunke4@gmail.com>
2026-04-15 22:55:50 +0530

    Add GitHub Actions workflow3

--- next.config.ts
+++ next.config.ts
@@ -5,15 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-
-  // 1. Updated to match your repository name (assumed to be 'portfolio')
   basePath: '/portfolio',
   assetPrefix: '/portfolio/',
-
-  // 2. Temporarily ignore code errors to force the deployment through
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
